c0c6abf0e338000a338d004c75e508060180f35d
Corklicense190 <Al02776632@tecmilenio.mx>
2025-03-19 09:25:51 -0600

    Segunda Respuesta de Andre
    Juas

--- holamundoDO.py
+++ holamundoDO.py
@@ -6,4 +6,5 @@ print("Ola ando jot")
 print("A cuatnos km estas baby?")
 print("Dormí 2 horas, soy un fakin loser")
 
-print("Mirenme soy imbreic, duermo 2 horas y me gustan las traumadas")
+print("Mirenme soy imbecil, duermo 2 horas y me gustan las traumadas")
+print("No me gusta este juego, Soid Intjilente porq armoh un cubo rubikc en 1 min")
