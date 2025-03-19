a08878e11b0d07267c21a9afa996cc86aa29c9b2
AVSL05 <161667346+AVSL05@users.noreply.github.com>
2025-03-19 09:35:13 -0600

    no molaba

--- holamundoDO.py
+++ holamundoDO.py
@@ -11,3 +11,5 @@ print("Esto lo leera el profe asi que comportense plox papus :v 👀 👀")
 print("https://images.app.goo.gl/qF1wsxXuPa5kMWeb9")
 
 print("Estoy en tu césped Dave el loco XdxDDXdddxxdXXDXDxdXD")
+
+print("https://www.google.com/imgres?imgurl=https://pbs.twimg.com/media/GhHI7R4XUAAXpt5.jpg&tbnid=PUpZvx1pUHco6M&vet=1&imgrefurl=https://x.com/Alan_Art1718/status/1878503442307518613&docid=2-85xAXFtehTBM&w=678&h=604&source=sh/x/im/m1/1&kgs=a0f4a2897845cb68#imgrc=9woJTndAQ8PLxM&imgdii=V9TRksef5ium8M")
